6aa2d01e845fe5c5bbfda11f4eb0ca41c405a2a0
michaelafm <105201811+michaelafm@users.noreply.github.com>
2022-11-15 13:39:53 +0000

    solve and test 5 GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -39,43 +39,80 @@ describe("/api/topics", () => {
 });
 
 describe("/api/articles", () => {
-    test("GET: 200 - sends an array of article objects to the user, each with author, title, article_id, topic, created_at, votes, comment_count properties", () => {
-      return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((res) => {
-          expect(res.body.articles).toEqual(expect.any(Array));
-          expect(res.body.articles.length).toBe(12);
-          res.body.articles.forEach((article) => {
-            expect(article).toEqual(
-              expect.objectContaining({
-                author: expect.any(String),
-                title: expect.any(String),
-                article_id: expect.any(Number),
-                topic: expect.any(String),
-                created_at: expect.any(String),
-                comment_count: expect.any(Number),
-                votes: expect.any(Number)
-              })
-            );
-          });
+  test("GET: 200 - sends an array of article objects to the user, each with author, title, article_id, topic, created_at, votes, comment_count properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toEqual(expect.any(Array));
+        expect(res.body.articles.length).toBe(12);
+        res.body.articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              comment_count: expect.any(Number),
+              votes: expect.any(Number),
+            })
+          );
         });
-    });
-    test("GET: 200 - sends an array of article objects where comment_count property is the total number of comments per article_id", () => {
-      return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((res) => {
-          expect(res.body.articles[0].comment_count).toBe(2);
-        });
-    });
-    test("GET: 200 - array of article objects is sorted by created_at date and time descending order by default", () => {
-      return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((res) => {
-          expect(res.body.articles).toBeSortedBy("created_at", {descending: true})
-          expect(res.body.articles[0].author).toBe('icellusedkars');
-        });
-    });
+      });
   });
+  test("GET: 200 - sends an array of article objects where comment_count property is the total number of comments per article_id", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles[0].comment_count).toBe(2);
+      });
+  });
+  test("GET: 200 - array of article objects is sorted by created_at date and time descending order by default", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+        expect(res.body.articles[0].author).toBe("icellusedkars");
+      });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  test("GET: 200 - returns an object to the user, with author (username from the users table), title, article_id, body, topic, created_at, and votes properties", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.article).toMatchObject({
+          author: "butter_bridge",
+          title: "Living in the shadow of a great man",
+          article_id: 1,
+          body: "I find this existence challenging",
+          topic: "mitch",
+          created_at: expect.any(String),
+          votes: 100,
+        });
+      });
+  });
+  test("GET: 404 - sends appropriate error message when given a valid but non-existent article_id", () => {
+    return request(app)
+      .get("/api/articles/1000")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Article ID not found");
+      });
+  });
+  test("GET: 400 - sends appropriate error message when given an invalid article_id", () => {
+    return request(app)
+      .get("/api/articles/invalid_id")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Invalid article ID");
+      });
+  });
+});
--- models/articles-models.js
+++ models/articles-models.js
@@ -1,14 +1,40 @@
-const db = require('../db/connection');
+const db = require("../db/connection");
 
 exports.selectArticles = () => {
-  return db.query(`
+  return db
+    .query(
+      `
   SELECT users.username AS author, title, articles.article_id, topic, articles.created_at, articles.votes, CAST(COUNT(comments.article_id) AS INT) AS comment_count
   FROM articles
   LEFT JOIN comments on articles.article_id = comments.article_id 
   LEFT JOIN users on articles.author = users.username
   GROUP BY users.username, title, articles.article_id, topic, articles.created_at, articles.votes
   ORDER BY articles.created_at DESC
-  ;`).then((result) => {
-    return result.rows;
-  });
+  ;`
+    )
+    .then((result) => {
+      return result.rows;
+    });
+};
+
+exports.selectArticleById = (article_id) => {
+  return db
+    .query(
+      `SELECT users.username AS author, title, article_id, body, topic, created_at, votes 
+    FROM articles 
+      JOIN users
+      ON articles.author = users.username
+      WHERE article_id = $1`,
+      [article_id]
+    )
+    .then((res) => {
+      if (res.rows[0] !== undefined) {
+        return res.rows[0];
+      } else {
+        return Promise.reject({
+          status: 404,
+          msg: "Article ID not found",
+        });
+      }
+    });
 };
