ca647a505b7fff573bd246463ca75c18d5137f4e
michaelafm <105201811+michaelafm@users.noreply.github.com>
2022-11-17 13:40:34 +0000

    9 GET users implemented and tested

---
+++ models/users-models.js
@@ -0,0 +1,12 @@
+const db = require('../db/connection');
+
+exports.selectUsers = () => {
+    return db
+      .query(
+        `SELECT username, name, avatar_url
+        FROM users;`
+      )
+      .then((result) => {
+        return result.rows;
+      });
+  };
